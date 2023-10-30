021c2135862b722bf8ef4eb427ee687c7e720ce2
Xage1 <kennethkiarie555@gmail.com>
2023-10-30 14:28:25 +0000

    Update for now

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,9 +5,11 @@
   * @tree: A pointer to the root node of the binary tree to traverse.
    * @func: A pointer to a function that takes an integer as a parameter.
     */
-    void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int)) {
-        if (tree == NULL || func == NULL) {
-                return; // Do nothing if tree or func is NULL
+    void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+    {
+        if (tree == NULL || func == NULL)
+        {
+                return;
                     }
 
                         binary_tree_postorder(tree->left, func);
