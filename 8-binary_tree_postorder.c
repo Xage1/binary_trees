d01cb44eb81953ff44875bf7a33b66f2b844df1c
Kenneth Muketha <89647552+Xage1@users.noreply.github.com>
2023-10-31 16:51:23 +0300

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_postorder - Traverse a binary tree using post-order traversal and apply a function to each node.
+* binary_tree_postorder - Traverse a binary tree using post-order traversal
 * @tree: A pointer to the root node of the binary tree to traverse.
 * @func: A pointer to a function that takes an integer as a parameter.
 */
