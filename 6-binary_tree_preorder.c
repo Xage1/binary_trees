a06550794bf492de55cd94c6ab136b759bd6c38a
Kenneth Muketha <89647552+Xage1@users.noreply.github.com>
2023-10-31 15:04:46 +0300

    Update 6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Traverse a binary tree using pre-order traversal and apply a function to each node.
-  * @tree: A pointer to the root node of the binary tree to traverse.
-   * @func: A pointer to a function that takes an integer as a parameter.
-    */
+* binary_tree_preorder - Traverse a binary tree using pre-order traversal and apply a function to each node.
+* @tree: A pointer to the root node of the binary tree to traverse.
+* @func: A pointer to a function that takes an integer as a parameter.
+*/
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
