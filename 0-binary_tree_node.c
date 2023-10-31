6571715f9b25a6f0ea8870ead3ae7304ed8b4d60
Kenneth Muketha <89647552+Xage1@users.noreply.github.com>
2023-10-31 15:09:34 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,16 +15,17 @@
 *         or NULL on failure (e.g., due to memory allocation issues).
 */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
 binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 if (new_node == NULL)
 {
-return NULL;
+return (NULL);
 }
 new_node->n = value;
 new_node->left = NULL;
 new_node->right = NULL;
 new_node->parent = parent;
 
-return new_node;
+return (new_node);
 }
