8e0d5f8290b4e4b2be96997a93f16ef28fbb78e7
Kenneth Muketha <89647552+Xage1@users.noreply.github.com>
2023-10-31 15:05:09 +0300

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - Traverse a binary tree using in-order traversal and apply a function to each node.
-  * @tree: A pointer to the root node of the binary tree to traverse.
-   * @func: A pointer to a function that takes an integer as a parameter.
-    */
+* binary_tree_inorder - Traverse a binary tree using in-order traversal and apply a function to each node.
+* @tree: A pointer to the root node of the binary tree to traverse.
+* @func: A pointer to a function that takes an integer as a parameter.
+*/
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
