c85abed1560d01e66cc231cc5ff2abcb0535e685
Kenneth Muketha <89647552+Xage1@users.noreply.github.com>
2023-10-31 16:30:06 +0300

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,19 +13,19 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 binary_tree_t *new_node;
 if (parent == NULL)
 {
-return NULL;
+return (NULL);
 }
-new_node =binary_tree_node(parent, value);
+new_node = binary_tree_node(parent, value);
 if (new_node == NULL)
 {
-return NULL;
+return (NULL);
 }
 new_node->right = parent->right;
 if (new_node->right != NULL)
 {
-new_node->right->parent =new_node;
+new_node->right->parent = new_node;
 }
 parent->right = new_node;
 
-return new_node;
+return (new_node);
 }
